53d6304e2bfa654cb2099e39effd35b3b147a643
Haoran Wang <vitowang1998@gmail.com>
2018-01-11 17:50:08 -0500

    Added more expalination about the basics in C.

--- basics/basics.c
+++ basics/basics.c
@@ -1,21 +1,25 @@
 // preprocessor instruction
 #include <stdio.h> // stodio.h is a file
+// stdio stands for "standard input/output library"
 
 
 // ANSI standard
-// "main" is the name of a function
+// "main" is the name of a function, the starting point of the program
 int main(void) // A function returns an integer and takes in nothing.
 { // Start of function body
     printf("Hello, World!\n"); // way of outputing strings in C.
-    
+          // ↑ character string/ string constant
     
     int num = 123;
     
     
     // details in designing: %d
-    printf("The number %d is such a good number,\n",123); // \n will move the cursor to the beginning of the next line.
+    printf("The number %d is such a good number\n",123); // \n will move the cursor to the beginning of the next line.
     
-    printf("The number %d is such a good number,",num); //
+    printf("The number %d is such a good number",num); //
+    
+    printf("Trying out a new command\t");
+    
     
     
     getchar(); // wait for a keystroke
