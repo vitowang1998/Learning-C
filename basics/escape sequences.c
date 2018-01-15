93fc1b3b12e4d20ef3d72de0ffd8a61c5aed98ac
Haoran Wang <vitowang1998@gmail.com>
2018-01-14 23:58:00 -0500

    Added a file about escape sequences in C.

---
+++ basics/escape sequences.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //     \n -> new line
+    printf("Nancy Mulligan is a beautiful song.\n");
+
+    //     \t -> insert a tab
+    printf("Nancy Mulligan is\d a beautiful song.\n", "is");
+
+    
+    
+    return 0;
+}
