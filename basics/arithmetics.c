e87e7d253469a55d1a3a141b371ec6ca77323f37
Haoran Wang <vitowang1998@gmail.com>
2018-01-15 20:40:26 -0500

    Added a file about the arithmetics in C.

---
+++ basics/arithmetics.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    // Addition
+    printf("%d\n", 1 + 1);
+    
+    // Substraction
+    printf("%d\n", 12 - 1);
+    
+    // Multiplication
+    printf("%d\n", 12 * 3);
+    
+    // Division: integer division -- discard all decimal parts
+    printf("%d\n", 12 / 3);
+    printf("%d\n", 13 / 3);
+    printf("%d\n", 14 / 3);
+    
+    return 0;
+}
