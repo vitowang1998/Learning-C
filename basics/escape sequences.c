2e2c30c713ed77c77977fedd4e39cbf1213ab1e2
Haoran Wang <vitowang1998@gmail.com>
2018-01-15 19:06:14 -0500

    Added more cases of escape sequences.

--- basics/escape sequences.c
+++ basics/escape sequences.c
@@ -6,9 +6,22 @@ int main(void)
     printf("Nancy Mulligan is a beautiful song.\n");
 
     //     \t -> insert a tab
-    printf("Nancy Mulligan is\d a beautiful song.\n", "is");
+    printf("Nancy Mulligan \tis a beautiful song.\n");
+    
+    //     \a -> alert
+    printf("Nancy Mulligan is a beautiful song.\a\n");
 
+    //     \" -> insert a special character "
+    //     also works for ', \, and ?
+    printf("\"Nancy Mulligan\" is a beautiful song.\n");
+
+    //     \v -> insert a verticle tab
+    printf("Nancy Mulligan \vis a beautiful song.\n");
     
+    //     \b -> moves the cursor back
+    printf("Nancy Mulligan \bis a beautiful song.\n");
     
+    //     \  -> the null type
+
     return 0;
 }
