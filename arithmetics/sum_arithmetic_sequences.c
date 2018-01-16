869c63f8f42e9f86ba9119522091cf9f2dc6f60b
Haoran Wang <vitowang1998@gmail.com>
2018-01-15 21:25:06 -0500

    Added a function to sum any arithmetic sequences with integer difference.

---
+++ arithmetics/sum_arithmetic_sequences.c
@@ -0,0 +1,7 @@
+// sum_arithmetic_sequences(first, last, num_of_terms) sums an
+// arithmetic sequence
+// requires: first ≤ last, difference be integer
+int sum_arithmetic_sequences(int first, int last, int num_of_terms) {
+    return (first + last) * num_of_terms / 2;
+}
+
