28125b7f1aa5bd07535a9851108deea13136b7af
Haoran Wang <vitowang1998@gmail.com>
2018-01-15 20:33:12 -0500

    Added the history of C.

---
+++ basics/history.c
@@ -0,0 +1,7 @@
+// history of C
+// Dennis Ritchie -> C (1969~ 1973) -> in order to make Unix more portable
+// C allows the developers to do some low-level operations like accessing the memory
+// C ensures that its code can be translate to "machine language"
+
+// C is used in coding operating systems. e.g. Windows, MacOS, Android, etc.
+
