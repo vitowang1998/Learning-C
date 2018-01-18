2df52ddeac2bfaea3b1938fcb12bdcaf84df3b72
Haoran Wang <vitowang1998@gmail.com>
2018-01-18 00:49:36 -0500

    Added a file to differentiate three types of statements in C.

---
+++ basics/statements.c
@@ -0,0 +1,15 @@
+// types of statements
+
+// 1. compound statements
+//    a block of multiple statements in curly braces
+
+// 2. expression statements
+//    statements that are used to make "side effects"
+
+// 3. Control flow statements
+//    statements that changes the order of execution of files
+//    e.g. return / break / if / else if / else
+
+// references
+// Control Flow Statements, Java Documentations. Oracle Corporation. (n.d.)
+// Retrieved from: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/flow.html  all copyrights belong to Oracle Corporation
