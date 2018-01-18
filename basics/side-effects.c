1e54e6bb05a1be7a703a287c1cd9a5768b1dbc34
Haoran Wang <vitowang1998@gmail.com>
2018-01-17 23:53:29 -0500

    Added a file about side-effects in C.

---
+++ basics/side-effects.c
@@ -0,0 +1,8 @@
+// functional programming does not have any side effects.
+
+// however, in imperative/object-oriented programming paradigm, we have side effects.
+
+// there are mainly two kinds of side effects
+
+// 1. I/O
+// 2. Mutation (memory modification)
