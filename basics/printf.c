b4d2aeda5cfa2b92607aa4618fff113d031f2966
Haoran Wang <vitowang1998@gmail.com>
2018-01-25 11:55:39 -0500

    Added a function about the return value of printf in C.

---
+++ basics/printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+// the return value of printf is the number of chars it prints
+int main(void) {
+
+    int num = 0;
+    
+    // will print "hello" and starts a new line
+    num = printf("hello\n");
+    
+    printf("%d\n", num);
+    
+    
+    
+    return 0;
+}
