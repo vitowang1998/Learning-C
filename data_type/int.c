80e407e0554b967439f9fa2a9e17bbfd47cee927
Haoran Wang <vitowang1998@gmail.com>
2018-01-28 01:52:58 -0500

    Added a file about the range of integer.

---
+++ data_type/int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// range of integer
+const int min_int = -2147483648;
+const int max_int = 2147483647;
+
+int main() {
+	printf("%d\n", min_int);
+	printf("%d\n", max_int);
+	return 0;
+}
