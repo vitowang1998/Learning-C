768736da361e8461b5614dc8526a273f9152978e
Haoran Wang <vitowang1998@gmail.com>
2018-02-01 22:42:15 -0500

    Added a function to test if adding two ints will cause overflow.

--- data_type/int.c
+++ data_type/int.c
@@ -4,6 +4,21 @@
 const int min_int = -2147483648;
 const int max_int = 2147483647;
 
+
+// can_add(a, b) tests if we can safely add integer a and b together
+//  without causing an overflow. It returns true if we can, false if
+//  not.
+bool can_add(int a, int b) {
+    if (a > 0 && b > (max_int - a)) {
+        return false;
+    } else if (a < 0 && b < (min_int - a)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 int main() {
 	printf("%d\n", min_int);
 	printf("%d\n", max_int);
