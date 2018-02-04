9da7ecd8f704c34651f0ec812a1483dab08dfe4c
Haoran Wang <vitowang1998@gmail.com>
2018-02-04 01:51:55 -0500

    Added two functions explaining the mechanism of abs() functions in C.

---
+++ math/abs.c
@@ -0,0 +1,19 @@
+// abs_int(n) returns the absolute value of an integer
+// O(1)
+int abs_int(int n) {
+    if (n >= 0) {
+        return n;
+    } else {
+        return -n;
+    }
+}
+
+// abs_double(n) returns the absolute value of a double
+// O(1)
+double abs_double(double n) {
+    if (n >= 0) {
+        return n;
+    } else {
+        return -n;
+    }
+}
