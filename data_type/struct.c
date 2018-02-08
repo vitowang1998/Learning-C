5469e5ec5bb6a45f6c241fa9ba55b871d2f1604d
Haoran Wang <vitowang1998@gmail.com>
2018-02-07 23:53:22 -0500

    Added a file about struct in C.

---
+++ data_type/struct.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct posn {
+    int x;
+    int y;
+};
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
+
+
+// Notes
+// When we pass a struct to a function in C,
+// we pass the entire struct to it, not a pointer to it
+
+
+
+
+
+
+
+
