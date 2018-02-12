d5c8ca25760b1daadfb813f8116a629c38494978
Haoran Wang <vitowang1998@gmail.com>
2018-02-11 23:41:23 -0500

    Added a line of explaination for dereference operator.

--- memory/pointer.c
+++ memory/pointer.c
@@ -3,3 +3,6 @@
 
 // The invalid pointer is denoted as NULL
 // If we try to dereference the NULL, our program will crash.
+
+// Indirection/Dereference Operator *
+// if p is a pointer, *p produces the value p points at
