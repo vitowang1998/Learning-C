d699059d2cdb0c5f98003f3e5b66cb1367d3b74b
Haoran Wang <vitowang1998@gmail.com>
2018-02-16 00:17:08 -0500

    Added a file to talk about the pass-by-value scheme of C.

---
+++ memory/pass_by_value.c
@@ -0,0 +1,13 @@
+// pass by value
+// In C, when we pass a parameter to a function, a copy of the very parameter is made and passed.
+// This way is called "pass by value"
+// This implies that when we pass a complicated struct to a function, a copy of the entire struct will be made, which may lead to a waste of stack memory.
+
+
+
+#include <stdio.h>
+
+int main(void) {
+    
+}
+
