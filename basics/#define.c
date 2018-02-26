6781d85135f31f1822d1d9484137c07dfb1eab3c
Haoran Wang <vitowang1998@gmail.com>
2018-02-26 15:06:56 -0500

    Added a demo file about the preprocessor directive #define.

---
+++ basics/#define.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// preprocessor directive
+// it finds all the "hello"s in the program and replace them with 146
+#define hello 146
+
+int main(void) {
+    printf("%d\n", hello);
+}
+
+
