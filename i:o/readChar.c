cb1ab854a480b7eebaf2a2a1ca6c055c2721fb8b
Haoran Wang <vitowang1998@gmail.com>
2018-02-25 19:57:26 -0500

    Added a function of reading in a char from user.

---
+++ i:o/readChar.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+// readInt(ignoreSpace) reads in a character from the user. It will
+//      ignore a white space if ignoreSpace is true. It will not ignore
+//      a white space if ignoreSpace is false.
+// requires: valid input.
+char readChar(bool ignoreSpace) {
+    char c = 'a';
+    int count = 0;
+    
+    if (ignoreSpace == true) {
+        count = scanf(" %c", &c);
+    } else {
+        count = scanf("%c", &c);
+    }
+    
+    if (count == 1) {
+        return c;
+    } else {
+        printf("Error: Invalid Input");
+    }
+    return c;
+}
+
+
+
+int main(void) {
+    char c = 'A';
+    printf("%c\n", c);
+    
+    c = readChar(false);
+    printf("%c\n", c);
+    
+   
+}
+
+
