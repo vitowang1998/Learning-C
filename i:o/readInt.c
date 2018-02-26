a4b1dbe079b998c470e80c71ec30df92a7dfb431
Haoran Wang <vitowang1998@gmail.com>
2018-02-25 19:33:50 -0500

    Added a function about reading in integers.

---
+++ i:o/readInt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+// readInt() reads in an integer from the user.
+// requires: valid input.
+int readInt(void) {
+    int i = 0;
+    int result = scanf("%d", &i);
+    if (result == 1) {
+        return i;
+    } else {
+        printf("Error. Invalid input.\n");
+        return -1;
+    }
+}
+
+
+
+int main(void) {
+    int i = 0;
+    printf("i = %d\n", i);
+    
+    i = readInt();
+    
+    printf("i = %d\n", i);
+}
+
+
