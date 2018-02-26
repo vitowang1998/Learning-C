4160e714e7cb5b8e17c91a224e3ef93f9bc17b85
Haoran Wang <vitowang1998@gmail.com>
2018-02-26 15:52:30 -0500

    Added a file about the scope in implementing modularization in C.

---
+++ modularization/scope.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// scope
+
+int globalNum = 10; // global scope
+static int staticNum = 12; // module scope
+// we should make the variables and functions static if they are not supposed
+//      to be provided.
+
+
+int main(void) {
+    
+}
+
