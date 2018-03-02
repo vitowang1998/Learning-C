489a6c4a29272973b3b16d6283824c0abd1adee6
Haoran Wang <vitowang1998@gmail.com>
2018-03-02 00:08:11 -0500

    Added a function about swapping two integers.

---
+++ math/swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <assert.h>
+// Unlike Pascal, C does not allow a function to return multiple values.
+// We use pointers when we have such needs.
+
+
+
+// swap(*a, *b) swaps the value of *a and *b
+// requires: a, b are valid pointers
+// effects: mutate the value of *a and *b
+void swap(int * a, int * b) {
+    assert(a);
+    assert(b);
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+int main(void) {
+
+}
+
