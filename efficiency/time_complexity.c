cb27dd75aa77d163e234ae3fad1b06f1116fe162
Haoran Wang <vitowang1998@gmail.com>
2018-03-19 22:21:35 -0400

    Adds a file explaining about time complexity in C.

---
+++ efficiency/time_complexity.c
@@ -0,0 +1,46 @@
+// Running Time
+// Denoted by O()
+
+// Big O notation
+// The running time is the most "dominant" term, without any coefficients
+// O(1000000) = O(1)
+// O(1924124192 * n^2) = O(n^2)
+
+
+// Addition
+// O(1) + O(1) = O(1)
+// O(1) + O(n) = O(n)
+// O(n) + O(n*logn) = O(n*logn)
+
+
+// Multiplication
+// O(1) * O(n) = O(n)
+// O(n) * O(logn) = O(n*logn)
+
+
+
+// O(1) < O(logn) < O(n) < O(n*logn) < O(n^2) < O(n^3) < O(n^n) < O(2^n)
+
+
+
+// Summation (Time efficiency of iterations)
+// ∑(i = 1 ~ logn) O(1) = logn
+// ∑(i = 1 ~ n) O(1) = n
+// ∑(i = 1 ~ n) O(n) = n^2
+
+
+// Arithmetic Sequences
+// ∑(i = 1 ~ n) O(i) = O((n^2 + n)/2) = O(n^2)
+// ∑(i = 1 ~ n) O(i^2) = O((2n^3 + 3n^2 + n) / 6) = O(n^3)
+
+
+
+
+
+
+
+
+
+
+
+
