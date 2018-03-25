a3b93f48dd5a1dda7b80df0b8633275f62842e1f
Haoran Wang <vitowang1998@gmail.com>
2018-03-25 13:51:12 -0400

    Adds a file about heap structure in memory.

---
+++ memory/heap.c
@@ -0,0 +1,11 @@
+// Heap
+
+// Heap is part of memory.
+// We can borrow some part of heap through "allocation" and free the borrowed part
+//      through "deallocation".
+
+// Heap is much bigger than stack, however, it still has a finite size.
+// When we try to borrow more memory than the heap can offer, the program will crash.
+
+
+
