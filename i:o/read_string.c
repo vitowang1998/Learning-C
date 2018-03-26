95da44344f04c51e1543399792e0a2b74bfaf2e3
Haoran Wang <vitowang1998@gmail.com>
2018-03-25 22:38:22 -0400

    Corrects the read_string() function.

--- i:o/read_string.c
+++ i:o/read_string.c
@@ -11,34 +11,24 @@
 // effects: allocate memory, mutate array
 // time: O(n) where n is the size of input
 char *read_string(void) {
-    char c;
-    // assert the IO is not empty
-    if (scanf("%c", &c) != 1) {
-        return NULL;
-    }
-    
-    int length = 1;
+    char c = 0;
+    int length = 0;
     int max_length = 1;
-    
     char *string = malloc(max_length * sizeof(char));
-    string[0] = c;
-    
     // reads the Input Stream
     while (true) {
         if (scanf("%c", &c) != 1) {
             break;
         }
-        length++;
-        
         if (length == max_length) {
             max_length *= 2;
             string = realloc(string, max_length * sizeof(char));
         }
+        length++;
         string[length - 1] = c;
     }
     
     length++;
-    string = realloc(string, length * sizeof(char));
     string[length] = '\0';
     return string;
 }
