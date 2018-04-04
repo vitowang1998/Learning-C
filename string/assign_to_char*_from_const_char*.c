d1ff7f52c6548f5785894f8e9343ca8dc6b9e0a3
Haoran Wang <vitowang1998@gmail.com>
2018-04-03 23:13:32 -0400

    Adds a better approach of assigning a char* from a const char*.

--- string/assign_to_char*_from_const_char*.c
+++ string/assign_to_char*_from_const_char*.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
     const char* str1 = "12415251643";
     printf("Str1 = %s\n", str1);
     char* str2 = NULL;
-    str2 = str1;
+    str2 = strdup(str1);
     printf("Str2 = %s\n", str2);
 }
