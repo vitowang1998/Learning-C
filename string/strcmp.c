86a59ccc3092d30988801f28c983cdbeb847f18a
Haoran Wang <vitowang1998@gmail.com>
2018-04-15 11:03:30 -0400

    Adds a file about strcmp function in C.

---
+++ string/strcmp.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+
+// my_strcmp(s1[], s2[]) returns 0 if str1[] str2[] are identical, 1 if str1 precedes
+//      str2, -1 if str1 follows str2
+// requires: str1[], str2[] are valid C style strings
+// time: O(n) where n = min(strlen(str1), strlen(str2))
+int my_strcmp(const char str1[], const char str2[]) {
+    assert(str1);
+    assert(str2);
+    int i = 0;
+    while (str1[i] == str2[i]) {
+        if (str1[i] == '\0' && str2[i] == '\0') {
+            return 0;
+        }
+        i++;
+    }
+    if (str1[i] > str2[i]) {
+        return 1;
+    }
+    return -1;
+}
