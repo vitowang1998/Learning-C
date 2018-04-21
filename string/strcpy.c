98f8a67a8c6b8ba021bb97b31076d300acd214a1
Haoran Wang <vitowang1998@gmail.com>
2018-04-21 00:20:46 -0400

    Adds a file about strcpy function in C.

---
+++ string/strcpy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// my_strcpy overwrites the contents of dest with contentes of scr
+// requires: dest has enough size
+// effects: mutates dest
+// time: O(n) where n = strlen(src)
+char *my_strcpy(char *dest, const char *src) {
+    int i = 0;
+    while (src[i]) {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i + 1] = '\0';
+    
+    return dest;
+}
+
+
+
+int main(void) {
+    char *str = "hello, world";
+    printf("%s\n", str);
+    char des[20];
+    
+    my_strcpy(des, str);
+    printf("%s\n", des);
+    
+}
