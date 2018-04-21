67215e104f7c4175b5293ee4aa9ab0875a391eff
Haoran Wang <vitowang1998@gmail.com>
2018-04-21 00:01:56 -0400

    Adds a file about buffer overrun in C.

---
+++ memory/buffer_overrun.c
@@ -0,0 +1,2 @@
+// buffer overrun: occurs when we scan a string to a string variable whose length is smaller than the string scanned. The size of the string variable is not enough to fit all the chars scanned.
+
